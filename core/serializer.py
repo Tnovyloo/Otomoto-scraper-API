35068207d5c0a002560270c6cadb31530346e452
TnovyLoo <loobrome@gmail.com>
2022-11-16 21:08:59 +0100

    Now i returned a more 'visible' data for 'user'

--- core/serializer.py
+++ core/serializer.py
@@ -17,9 +17,16 @@ from .models import *
 #         fields = ['url', 'car']
 
 class CarSerializer(serializers.ModelSerializer):
+    brand = serializers.CharField(source="brand.brand")
+    fuel = serializers.CharField(source="fuel.fuel_type")
+    gearbox = serializers.CharField(source="gearbox.gearbox_type")
+    bodytype = serializers.CharField(source="body_type.bodytype")
+    color = serializers.CharField(source="color.color")
+
     class Meta:
         model = Car
         fields = "__all__"
+        # depth = 1
 
 class SearchedURLSerializer(serializers.ModelSerializer):
     cars = CarSerializer(many=True, read_only=True)
